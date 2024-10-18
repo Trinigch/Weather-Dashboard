b9aec5bf440687203b322d542cc6f370f7be6958
Trinigch <trinigch@gmail.com>
2024-10-18 18:36:49 -0500

    fixing route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,36 @@
 
 import dotenv from 'dotenv';
-import express from 'express';
+
 
 import routes from './routes/index.js';
 dotenv.config();
 
 // Import the routes
-//import routes from './routes/index.ts';
+import express, { type Request, type Response } from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+//import TipService from './service/TipService.js';
+//import FeedbackService from './service/FeedbackService.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
-
-//app.use(express.static('client/dist'));  // Serv static files for "dist"
-app.use(express.static('/'));
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+// Serves static files in the entire client's dist folder
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// TODO: Serve static files of entire client dist folder
+app.use(express.static('../client/dist'));
+
+// This view route is a GET route for the home page
+app.get('/', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
+
+//import routes from './routes/index.ts';
+// TODO: Implement middleware for parsing JSON and urlencoded form data
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 

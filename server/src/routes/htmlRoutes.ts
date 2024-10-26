d843754b2afbb095766f066bf8c7ea39a8976ee6
Trinigch <trinigch@gmail.com>
2024-10-26 16:39:41 -0500

    fix bug of index route

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,13 +1,15 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import {type Request, type Response } from 'express';
 import { Router } from 'express';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-router.get('/', (_, res) => {
-    // Envía el archivo index.html ubicado en el directorio público o raíz
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html')); //  'index.html' in folder'client'
-  });
+
+router.get('/', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+});
+
 export default router;
